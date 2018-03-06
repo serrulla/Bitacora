b8b8768386e3d877c879faa08bb2b65ced02183f
Pau Fernández <pau.fernandez@upc.edu>
2018-03-06 11:17:59 +0100

    Mostrem una llista d'elements ficticis.

--- app/src/main/java/fernandez/pau/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/fernandez/pau/shoppinglist/ShoppingListActivity.java
@@ -2,12 +2,29 @@ package fernandez.pau.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ShoppingListActivity extends AppCompatActivity {
+
+    private ListView list;
+    private ArrayList<String> items; // Model de dades
+    private ShoppingListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
+
+        // Omplim el model de dades
+        items = new ArrayList<>();
+        items.add("Patates");
+        items.add("Paper WC");
+        items.add("Ketchup");
+
+        list = (ListView) findViewById(R.id.list);
+        adapter = new ShoppingListAdapter(this, R.layout.shopping_item, items);
+        list.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/fernandez/pau/shoppinglist/ShoppingListAdapter.java
@@ -0,0 +1,40 @@
+package fernandez.pau.shoppinglist;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+
+import java.util.List;
+
+/**
+ * Created by pablofd on 06/03/2018.
+ */
+
+public class ShoppingListAdapter extends ArrayAdapter<String> {
+    public ShoppingListAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<String> objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // 1. Crear un nou View si és necessari (no cal si convertView no és null)
+        View root = convertView; // arrel d'un item de la llista
+        if (root == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            root = inflater.inflate(R.layout.shopping_item, parent, false);
+        }
+
+        CheckBox checkBox = (CheckBox) root.findViewById(R.id.checkBox);
+        String item = getItem(position);
+        checkBox.setText(item);
+
+        return root;
+    }
+}

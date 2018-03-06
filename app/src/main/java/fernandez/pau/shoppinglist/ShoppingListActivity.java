76fef54eaad49792ab05d5106b49b186a340c264
Pau Fernández <pau.fernandez@upc.edu>
2018-03-06 11:40:30 +0100

    Podem afegir elements a la llista.

--- app/src/main/java/fernandez/pau/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/fernandez/pau/shoppinglist/ShoppingListActivity.java
@@ -2,6 +2,8 @@ package fernandez.pau.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -11,6 +13,7 @@ public class ShoppingListActivity extends AppCompatActivity {
     private ListView list;
     private ArrayList<String> items; // Model de dades
     private ShoppingListAdapter adapter;
+    private EditText new_item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +27,17 @@ public class ShoppingListActivity extends AppCompatActivity {
         items.add("Ketchup");
 
         list = (ListView) findViewById(R.id.list);
+        new_item = (EditText) findViewById(R.id.new_item);
+
         adapter = new ShoppingListAdapter(this, R.layout.shopping_item, items);
         list.setAdapter(adapter);
+    }
+
+    public void onAddItem(View view) {
+        String item = new_item.getText().toString();
+        items.add(item);
+        adapter.notifyDataSetChanged();
+        new_item.setText("");
+        list.smoothScrollToPosition(items.size()-1);
     }
 }
